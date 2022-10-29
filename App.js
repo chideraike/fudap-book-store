29db4130c0915486c9a05fae10b46e7350f6886f
Chidera Ike <chideraike.s@gmail.com>
2022-10-29 18:30:24 +0000

    :sparkles: added loading indicator while fetching data from the api

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { ActivityIndicator, FlatList, Text, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Text, View, SafeAreaView } from 'react-native'
 
 export default function App() {
   const [isLoading, setLoading] = useState(true)
-  const [data, setData] = useState([])  
- 
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('https://fudap-books-api.herokuapp.com/books/')
+      .then(response => {
+        if (response.ok) return response.json()
+        throw response
+      })
+      .then(data => {
+        setData(data)
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <ActivityIndicator size="large" color="#0000ff" style={{ flex: 1 }} />
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-    </View>)
- 
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text>
+        Fudap Book Store
+      </Text>
+    </SafeAreaView>
+  )
 }
 
